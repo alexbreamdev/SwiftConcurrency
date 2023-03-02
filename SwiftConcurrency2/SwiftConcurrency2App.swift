bc2c6740bf55532a879a4cd4580ca425cd766cf1
Oleksii Leshchenko <84497187+alexbreamdev@users.noreply.github.com>
2023-03-02 12:43:41 +0200

    Initial Commit

---
+++ SwiftConcurrency2/SwiftConcurrency2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrency2App.swift
+//  SwiftConcurrency2
+//
+//  Created by Oleksii Leshchenko on 02.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrency2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
