83cbac1bc619bd6ef67e3d6234bb8761bb234d2e
Oleksii Leshchenko <84497187+alexbreamdev@users.noreply.github.com>
2023-04-13 19:35:47 +0300

    sendable done

--- SwiftConcurrency2/SendableView.swift
+++ SwiftConcurrency2/SendableView.swift
@@ -11,12 +11,48 @@ class SendableViewModel: ObservableObject {
     let manager = CurrenUserManager()
     
     func updateCurrentUserInfo() async {
-        await manager.updateDatabase()
+        // == Struct in concurrent environment ==
+//        let info = MyUserInfo(name: "USER INFO")
+//        await manager.updateDatabase(userInfo: info)
+        
+        // == Class in concurrent environment ==
+        let info = MyClassUserInfo(name: "Class User Info")
+        await manager.updateDatabase(userInfo: info)
+    }
+}
+// struct immutable and thread safe by default
+// to confirm that MyUserInfo is thread safe need to conform it to Sendable,
+// so it can be used in concurrent code
+struct MyUserInfo: Sendable {
+    let name: String
+}
+
+// 1. non-final class can't conform to sendable
+// 2. class fields have to be constants (let) OR
+// (2.) add @unchecked -> prevents compiler warning
+final class MyClassUserInfo: @unchecked Sendable {
+    private var name: String
+    // add lock to mutate var
+    let lock = DispatchQueue(label: "com.myapp.MyClassUserInfo")
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func updateName(name: String) {
+        // mutating var on lock queue
+        lock.async {
+            self.name = name
+        }
     }
 }
 
 actor CurrenUserManager {
-    func updateDatabase() {
+    func updateDatabase(userInfo: MyUserInfo) {
+        
+    }
+    
+    func updateDatabase(userInfo: MyClassUserInfo) {
         
     }
 }
